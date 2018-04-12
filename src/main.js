710ce26c2301559af17f8ce2a705f40cd7cb51e1
Mathieu Brédif <mathieu.bredif@ign.fr>
2018-04-12 02:38:15 +0200

    wip: debug in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import * as itowns from 'itowns';
 import ROSProvider from './ROSProvider';
+import * as THREE from 'three';
 
-let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 25000000 };
+let positionOnGlobe = { longitude: 8.4317799209025, latitude: 49.014364070464, altitude: 1000 };
 let viewerDiv = document.getElementById('viewerDiv');
 let globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe);
 
@@ -19,7 +20,7 @@ promises.push(globeView.addLayer({
     ros: { url: 'ws://localhost:9090' },
     topic: '/kitti/oxts/gps/fix',
     keep: 50,
-    color: 0xff0000
+    material: {color: 0xff00dd, linewidth: 10}    
 }));
 
 globeView.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, () => {
@@ -28,3 +29,21 @@ globeView.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, () => {
     })
 });
 
+globeView.wgs84TileLayer.visible = false;
+
+console.log('itowns', itowns);
+
+var sphere = new THREE.Mesh(
+        new THREE.SphereGeometry( 100000, 32, 32 ),
+        new THREE.MeshBasicMaterial( {color: 0xffff00, depthTest: false} )
+    );
+
+
+var coords = new itowns.Coordinates('EPSG:4326', positionOnGlobe.longitude, positionOnGlobe.latitude, positionOnGlobe.altitude);
+var p = coords.as('EPSG:4978').xyz();
+sphere.position.set(p.x, p.y, p.z);
+sphere.updateMatrix();
+globeView.scene.add(sphere);
+
+console.log(globeView.camera);
+console.log(sphere.position);
